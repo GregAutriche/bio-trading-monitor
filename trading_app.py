cfb7d5c1c8bf4e505dafb13962862fe9c890b8a3
GregAutriche <gregor.lutz@outlook.com>
2026-02-15 12:23:43 +0100

    Update trading_app.py

--- trading_app.py
+++ trading_app.py
@@ -1,4 +1,3 @@
-*** code ***
 import streamlit as st
 import yfinance as yf
 import plotly.graph_objects as go
@@ -155,6 +154,7 @@ for label, sym in tickers:
         st.write(f"ℹ️ {label}: Daten werden geladen...")
 
 st.markdown("---")
+
 
 
 
